ecbcbda98036122288b4678cf2b33e600c0f5aca
zhanghao <498861397@qq.com>
2021-05-10 19:53:08 +0800

    创建项目

--- springBoot/src/main/java/cn/happyonion801/study/springboot/controller/Hello.java
+++ springBoot/src/main/java/cn/happyonion801/study/springboot/controller/Hello.java
@@ -1,8 +1,5 @@
 package cn.happyonion801.study.springboot.controller;
 
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
 
     @RequestMapping("/")
-    public String Hello(){
+    public String HelloPage(){
         return "Hello SpringBoot";
     }
 }
